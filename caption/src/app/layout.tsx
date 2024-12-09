0f6bf33a5859447ea6fb503c81652919791ec57f
Himanshu Chauhan <himanshuchau423@gmail.com>
2024-12-09 12:44:05 +0530

    on page SEO done

--- caption/src/app/layout.tsx
+++ caption/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "@/redux/provider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Booleanix - AI Caption Generator",
@@ -15,10 +16,74 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const breadcrumbSchema = {
+    "@context": "https://schema.org/",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Login",
+        item: "https://www.booleanix.com/login",
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Sign Up",
+        item: "https://www.booleanix.com/sign-up",
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: "Privacy Policy",
+        item: "https://www.booleanix.com/privacy-policy",
+      },
+      {
+        "@type": "ListItem",
+        position: 4,
+        name: "Terms & Conditions",
+        item: "https://www.booleanix.com/terms-and-conditions",
+      },
+      {
+        "@type": "ListItem",
+        position: 5,
+        name: "Contact Us",
+        item: "https://www.booleanix.com/contact-us",
+      },
+    ],
+  };
+
   return (
     <html lang="en">
       <head>
-      <link rel="canonical" href="https://www.booleanix.com/" />
+        <link rel="canonical" href="https://www.booleanix.com/" />
+        <Script id="breadcrumb-schema" type="application/ld+json">
+          {JSON.stringify(breadcrumbSchema)}
+        </Script>
+        <meta property="og:title" content="Booleanix - AI Caption Generator" />
+        <meta property="og:site_name" content="Booleanix" />
+        <meta property="og:url" content="https://www.booleanix.com" />
+        <meta
+          property="og:description"
+          content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
+        />
+        <meta property="og:type" content="website">
+          <meta
+            property="og:image"
+            content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
+          />
+        </meta>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@booleanix" />
+        <meta name="twitter:title" content="Booleanix - AI Caption Generator" />
+        <meta
+          name="twitter:description"
+          content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
+        />
       </head>
       <body>
         <Providers>
---
+++ caption/src/app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date('2024-12-09T06:42:54+00:00');
+  return [
+    {
+      url: 'https://www.booleanix.com/login',
+      lastModified,
+      priority: 1.0,
+    },
+    {
+      url: 'https://www.booleanix.com/sign-up',
+      lastModified,
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.booleanix.com/privacy-policy',
+      lastModified,
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.booleanix.com/terms-and-conditions',
+      lastModified,
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.booleanix.com/contact-us',
+      lastModified,
+      priority: 0.8,
+    },
+  ];
+}
