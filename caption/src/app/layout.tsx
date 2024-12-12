c221b61b9a1e897b4d3518fa272c2f04ed4202f2
Himanshu Chauhan <himanshuchau423@gmail.com>
2024-12-12 10:50:22 +0530

    removed login and  signup

--- caption/src/app/layout.tsx
+++ caption/src/app/layout.tsx
@@ -23,30 +23,18 @@ export default function RootLayout({
       {
         "@type": "ListItem",
         position: 1,
-        name: "Login",
-        item: "https://www.booleanix.com/login",
-      },
-      {
-        "@type": "ListItem",
-        position: 2,
-        name: "Sign Up",
-        item: "https://www.booleanix.com/sign-up",
-      },
-      {
-        "@type": "ListItem",
-        position: 3,
         name: "Privacy Policy",
         item: "https://www.booleanix.com/privacy-policy",
       },
       {
         "@type": "ListItem",
-        position: 4,
+        position: 2,
         name: "Terms & Conditions",
         item: "https://www.booleanix.com/terms-and-conditions",
       },
       {
         "@type": "ListItem",
-        position: 5,
+        position: 3,
         name: "Contact Us",
         item: "https://www.booleanix.com/contact-us",
       },
@@ -56,34 +44,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-  <link rel="canonical" href="https://www.booleanix.com/" />
-  <Script id="breadcrumb-schema" type="application/ld+json">
-    {JSON.stringify(breadcrumbSchema)}
-  </Script>
-  <meta property="og:title" content="Booleanix - AI Caption Generator" />
-  <meta property="og:site_name" content="Booleanix" />
-  <meta property="og:url" content="https://www.booleanix.com" />
-  <meta
-    property="og:description"
-    content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
-  />
-  <meta property="og:type" content="website" />
-  <meta
-    property="og:image"
-    content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
-  />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="@booleanix" />
-  <meta name="twitter:title" content="Booleanix - AI Caption Generator" />
-  <meta
-    name="twitter:description"
-    content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
-  />
-  <meta
-    name="twitter:image"
-    content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
-  />
-</head>
+        <link rel="canonical" href="https://www.booleanix.com/" />
+        <Script id="breadcrumb-schema" type="application/ld+json">
+          {JSON.stringify(breadcrumbSchema)}
+        </Script>
+        <meta property="og:title" content="Booleanix - AI Caption Generator" />
+        <meta property="og:site_name" content="Booleanix" />
+        <meta property="og:url" content="https://www.booleanix.com" />
+        <meta
+          property="og:description"
+          content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@booleanix" />
+        <meta name="twitter:title" content="Booleanix - AI Caption Generator" />
+        <meta
+          name="twitter:description"
+          content="Booleanix is an AI-powered platform that simplifies caption creation, delivering creative captions for social media, marketing, and personal use."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.booleanix.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.958c6f9f.png&w=1920&q=75"
+        />
+      </head>
 
       <body>
         <Providers>
--- caption/src/app/sitemap.ts
+++ caption/src/app/sitemap.ts
@@ -4,19 +4,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date('2024-12-09T06:42:54+00:00');
   return [
     {
-      url: 'https://www.booleanix.com/login',
-      lastModified,
-      priority: 1.0,
-    },
-    {
-      url: 'https://www.booleanix.com/sign-up',
-      lastModified,
-      priority: 0.8,
-    },
-    {
       url: 'https://www.booleanix.com/privacy-policy',
       lastModified,
-      priority: 0.8,
+      priority: 1,
     },
     {
       url: 'https://www.booleanix.com/terms-and-conditions',
